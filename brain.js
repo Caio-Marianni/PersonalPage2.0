4573ab0185dc972c42a7fe943c67669db4d58c1d
Caio Marianni <caiomarianni@gmail.com>
2024-03-27 22:39:25 -0300

    sem nada aqui

--- brain.js
+++ brain.js
@@ -1,21 +1,19 @@
 // BACKGROUND FLOW FIELD --------------------------------------------
 let particles = [];
-const num = 500;
+
+const num = 100;
 
 const noiseScale = 0.01/2;
 
 function setup() {
-//   IF THE BACKGROUND NEED TO BE A LONGER BACKGROUND -> 5 is the number off sections in the page
-//   createCanvas(window.innerWidth * 5, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight);
   createCanvas(window.innerWidth, window.innerHeight);
   for(let i = 0; i < num; i ++) {
     particles.push(createVector(random(width), random(height)));
   }
   
   stroke(1);
-  // For a cool effect try uncommenting this line
-  // And comment out the background() line in draw
-  stroke('#f43d15');
+  stroke('#3ecc3e');
   clear();
 
 }
@@ -36,65 +34,19 @@ function draw() {
       p.y = random(height);
     }
   }
-}9
+}
 function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
 }
 
 // NAV ELEMENTS SELECT-----------------------------------------------
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("main #navbar ul li a");
-
-window.addEventListener("wheel", (evento) => {
-  // console.log("Dados do pageXOffSet e scrollX ->")
-  // console.log(pageXOffset)
-  // console.log(scrollX)
-  evento.preventDefault();
-  scrollContainer.scrollLeft += evt.deltaY;
-  // let current = "";
-  // sections.forEach((section) => {
-  //   const sectionHorizontal = section.offsetLeft;
-  //   const sectionWidth = section.clientWidth;
-  //   if (pageXOffset >= sectionHorizontal - sectionWidth / 3) {
-  //     current = section.getAttribute("id");
-  //   }
-  // });
-
-  // navLi.forEach((a ) => {
-  //   a.classList.remove("active");
-  //   if (a.classList.contains(current)) {
-  //     a.classList.add("active");
-  //   }
-  // });
-});
-
-// const sections = document.querySelectorAll("section");
-// const navLi = document.querySelectorAll("#navbar .text-container ul li a");
-// window.addEventListener("scroll", () => {
-//   let current = "";
-//   sections.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     const sectionHeight = section.clientHeight;
-//     if (pageYOffset >= sectionTop - sectionHeight / 3) {
-//       current = section.getAttribute("id");
-//     }
-//   });
-
-//   navLi.forEach((a ) => {
-//     a.classList.remove("active");
-//     if (a.classList.contains(current)) {
-//       a.classList.add("active");
-//     }
-//   });
-// });
-
-
-
-
+function change() {
+  document.getElementById('radio').checked = true;
+}
 //SCROLL TRANSLATE--------------------------------------------------
-const scrollContainer = document.querySelector("main");
+// const scrollContainer = document.querySelector("main");
 
-scrollContainer.addEventListener("wheel", (evt) => {
-      evt.preventDefault();
-      scrollContainer.scrollLeft += evt.deltaY;
-});
+// scrollContainer.addEventListener("wheel", (evt) => {
+//       evt.preventDefault();
+//       scrollContainer.scrollLeft += evt.deltaY;
+// });
